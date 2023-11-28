c9beb34015b52f6b5ec5c29ef0c9ad469f85014c
EveloCode <102839813+prabhasha2006@users.noreply.github.com>
2023-11-28 20:32:27 +0530

    😊💕🤣😍EveloCore

--- settings.js
+++ settings.js
@@ -1,4 +1,5 @@
 const settingsBox = () => {
     document.getElementsByClassName("settings-box")[0].classList.toggle('settings-box-active')
     document.getElementsByClassName("set-button")[0].classList.toggle('settings-button-inactive')
+    document.getElementsByClassName("back-button")[0].classList.toggle('back-button-active')
 }
